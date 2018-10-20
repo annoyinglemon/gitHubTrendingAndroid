1dc640c387ec69eabdb3aa8e2bbedb252d2a741e
Kurt Lemond <kurtlemond@gmail.com>
2018-10-20 16:58:21 -0400

    Created the projects cache implementation for cache module.

--- Cache/src/main/java/io/caster/tutorial/cache/dao/ConfigDao.kt
+++ Cache/src/main/java/io/caster/tutorial/cache/dao/ConfigDao.kt
@@ -6,12 +6,13 @@ import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import io.caster.tutorial.cache.db.ConfigConstants
 import io.caster.tutorial.cache.model.Config
+import io.reactivex.Flowable
 
 @Dao
 abstract class ConfigDao {
 
     @Query(ConfigConstants.QUERY_CONFIG)
-    abstract fun getConfig(): Config
+    abstract fun getConfig(): Flowable<Config>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insertConfig(config: Config)
